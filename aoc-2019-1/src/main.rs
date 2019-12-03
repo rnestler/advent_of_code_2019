1c194cd5ef4eaaffacb5b532ab58da2d1bd5c76c
Raphael Nestler <raphael.nestler@gmail.com>
2019-12-03 13:13:52 +0100

    Add day1 part2 solution

--- aoc-2019-1/src/main.rs
+++ aoc-2019-1/src/main.rs
@@ -1,19 +1,34 @@
 use std::fs::File;
 use std::io::prelude::*;
 
-fn get_required_fuel(mass: u32) -> u32 {
+fn get_required_fuel(mass: i32) -> i32 {
     mass / 3 - 2
+}
+
+fn get_required_fuel_part2(mass: i32) -> i32 {
+    let fuel = get_required_fuel(mass);
+    if fuel <= 0 {
+        0
+    } else {
+        fuel + get_required_fuel_part2(fuel)
+    }
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut file = File::open("input.txt")?;
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
-    let total: u32 = contents
+    let total: i32 = contents
         .lines()
         .map(|line| get_required_fuel(line.parse().expect("Parsing failed")))
         .sum();
 
     println!("result part1: {}", total);
+
+    let total: i32 = contents
+        .lines()
+        .map(|line| get_required_fuel_part2(line.parse().expect("Parsing failed")))
+        .sum();
+    println!("result part2: {}", total);
     Ok(())
 }
