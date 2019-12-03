75c46c41c4b2fa91f2b732d07993b2a87cf7c751
Raphael Nestler <raphael.nestler@gmail.com>
2019-12-03 13:05:30 +0100

    Add solution for day1 part1

--- aoc-2019-1/src/main.rs
+++ aoc-2019-1/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::prelude::*;
+
+fn get_required_fuel(mass: u32) -> u32 {
+    mass / 3 - 2
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut file = File::open("input.txt")?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    let total: u32 = contents
+        .lines()
+        .map(|line| get_required_fuel(line.parse().expect("Parsing failed")))
+        .sum();
+
+    println!("result part1: {}", total);
+    Ok(())
 }
