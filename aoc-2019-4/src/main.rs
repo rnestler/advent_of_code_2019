e8670dcaa3423973fec7cb1958560171d05ebb45
Raphael Nestler <raphael.nestler@gmail.com>
2019-12-04 07:51:28 +0100

    Solution for day4 part 1

--- aoc-2019-4/src/main.rs
+++ aoc-2019-4/src/main.rs
@@ -1,3 +1,26 @@
+fn check_pw(pw: u32) -> bool {
+    let mut old_digit = b'0';
+    let mut two_conservative_same = false;
+    let mut digits_increasing = true;
+    let pw_str = format!("{}", pw).into_bytes();
+    for d in 0..6 {
+        if pw_str[d] == old_digit {
+            two_conservative_same = true;
+        }
+        if pw_str[d] < old_digit {
+            digits_increasing = false
+        }
+        old_digit = pw_str[d];
+    }
+    two_conservative_same && digits_increasing
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut count = 0;
+    for pw in 372037..905157 {
+        if check_pw(pw) {
+            count += 1;
+        }
+    }
+    println!("result part1: {}", count);
 }
