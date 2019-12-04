2d0bde2aa0cb799dce559e3fd280589f4a5b0985
Raphael Nestler <raphael.nestler@gmail.com>
2019-12-04 09:09:06 +0100

    Solution for day4 part 2

--- aoc-2019-4/src/main.rs
+++ aoc-2019-4/src/main.rs
@@ -15,12 +15,25 @@ fn check_pw(pw: u32) -> bool {
     two_conservative_same && digits_increasing
 }
 
-fn main() {
-    let mut count = 0;
-    for pw in 372037..905157 {
-        if check_pw(pw) {
-            count += 1;
+fn check_pw_part2(pw: u32) -> bool {
+    let mut consecutive_count = [0; 10];
+    let pw_str = format!("{}", pw).into_bytes();
+    for digit in 0..10u8 {
+        for d in 0..6 {
+            if pw_str[d] == digit + b'0' {
+                consecutive_count[digit as usize] += 1;
+            }
         }
     }
-    println!("result part1: {}", count);
+    consecutive_count.contains(&2)
+}
+
+fn main() {
+    let part1: Vec<_> = (372037..905157).filter(move |pw| check_pw(*pw)).collect();
+    println!("result part1: {}", part1.len());
+
+    println!(
+        "result part2: {}",
+        part1.iter().filter(|pw| check_pw_part2(**pw)).count()
+    );
 }
