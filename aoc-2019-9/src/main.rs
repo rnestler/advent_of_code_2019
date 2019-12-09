f34b611097dc35dd36701cb42ba7934ef05b1325
Raphael Nestler <raphael.nestler@gmail.com>
2019-12-09 21:12:07 +0100

    Solution for day 9 part 2

--- aoc-2019-9/src/main.rs
+++ aoc-2019-9/src/main.rs
@@ -1,9 +1,16 @@
-use intcode_computer::{Machine, StepResult};
+use intcode_computer::Machine;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let code = Machine::read_code("input.txt")?;
-    let mut machine = Machine::new(code);
+
+    let mut machine = Machine::new(code.clone());
     let _ = machine.run_with_input(1);
+    let result = machine.drain_output();
+
+    println!("result: {:?}", result);
+
+    let mut machine = Machine::new(code);
+    let _ = machine.run_with_input(2);
     let result = machine.drain_output();
 
     println!("result: {:?}", result);
