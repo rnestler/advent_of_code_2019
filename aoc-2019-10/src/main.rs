cb198dc140af57dda308c02102cbf1bd451d1cb7
Raphael Nestler <raphael.nestler@gmail.com>
2019-12-10 15:13:16 +0100

    Solution for day 10 part 2

--- aoc-2019-10/src/main.rs
+++ aoc-2019-10/src/main.rs
@@ -7,6 +7,12 @@ struct Pos {
     y: i32,
 }
 
+#[derive(Debug, Copy, Clone, PartialEq, PartialOrd)]
+struct AngleDistance {
+    angle: f64,
+    distance: f64,
+}
+
 impl Pos {
     pub fn new(x: i32, y: i32) -> Self {
         Pos { x, y }
@@ -25,6 +31,21 @@ impl Pos {
             (diff.x as f64 / len * 1000.).round() as i32,
             (diff.y as f64 / len * 1000.).round() as i32,
         )
+    }
+
+    pub fn get_angle_distance(&self, other: Self) -> AngleDistance {
+        let diff = (
+            self.x as f64 - other.x as f64,
+            self.y as f64 - other.y as f64,
+        );
+
+        let mut angle = diff.1.atan2(diff.0);
+        // move last quadrant (-pi3/4..-pi/2) at the end
+        if angle < -std::f64::consts::PI / 2.0 {
+            angle += 2.0 * std::f64::consts::PI;
+        }
+        let distance = (diff.0 * diff.0 + diff.1 * diff.1).sqrt();
+        AngleDistance { angle, distance }
     }
 }
 
@@ -66,11 +87,70 @@ fn get_asteroids(input: &str) -> Vec<Pos> {
     asteroids
 }
 
+fn get_200th_vaporized(asteroids: Vec<Pos>, battle_station: Pos) -> Pos {
+    #[derive(Debug, Clone)]
+    struct Asteroid {
+        pos: Pos,
+        angle_distance: AngleDistance,
+    }
+    let mut asteroids: Vec<_> = asteroids
+        .iter()
+        .map(|a| Asteroid {
+            pos: *a,
+            angle_distance: a.get_angle_distance(battle_station),
+        })
+        .collect();
+    asteroids.sort_by(|a, b| {
+        a.angle_distance
+            .distance
+            .partial_cmp(&b.angle_distance.distance)
+            .unwrap()
+    });
+    asteroids.sort_by(|a, b| {
+        a.angle_distance
+            .angle
+            .partial_cmp(&b.angle_distance.angle)
+            .unwrap()
+    });
+
+    loop {
+        let mut n_vaporized = 0;
+        let mut old_angle = -10.0;
+        let asteroids_copy = asteroids.clone();
+        let mut to_remove = vec![];
+        for (i, a) in asteroids_copy.iter().enumerate() {
+            if a.angle_distance.angle == old_angle {
+                continue;
+            }
+            old_angle = a.angle_distance.angle;
+            n_vaporized += 1;
+            if n_vaporized == 200 {
+                return a.pos;
+            }
+            to_remove.push(i);
+        }
+        for i in &to_remove {
+            asteroids.remove(*i);
+        }
+        if asteroids.is_empty() {
+            panic!("should not happen");
+        }
+    }
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let content = read("input.txt")?;
     let asteroids = get_asteroids(&str::from_utf8(&content)?);
     let visible = get_visible_asteroids(&asteroids);
-    println!("Result {:?}", visible.iter().max());
+    let (pos, max) = visible.iter().enumerate().max_by_key(|v| v.1).unwrap();
+    println!("Result {:?}", max);
+
+    let battle_station = asteroids[pos];
+    let mut asteroids = asteroids;
+    asteroids.remove(pos);
+
+    let asteroid = get_200th_vaporized(asteroids, battle_station);
+    println!("Result part 2: {}", asteroid.x * 100 + asteroid.y);
     Ok(())
 }
 
@@ -84,6 +164,27 @@ mod tests {
 ....#
 ...##
 ";
+
+    const INPUT_BIG: &str = ".#..##.###...#######
+##.############..##.
+.#.######.########.#
+.###.#######.####.#.
+#####.##.#.##.###.##
+..#####..#.#########
+####################
+#.####....###.#.#.##
+##.#################
+#####.##.###..####..
+..######..##.#######
+####.##.####...##..#
+.#####..#.######.###
+##...#.##########...
+#.##########.#######
+.####.#.###.###.#.##
+....##.##.###..#####
+.#.#.###########.###
+#.#.#.#####.####.###
+###.##.####.##.#..##";
 
     #[test]
     fn test_get_asteroids() {
@@ -168,28 +269,24 @@ mod tests {
 
     #[test]
     fn test_get_visible_max_4() {
-        let input = ".#..##.###...#######
-##.############..##.
-.#.######.########.#
-.###.#######.####.#.
-#####.##.#.##.###.##
-..#####..#.#########
-####################
-#.####....###.#.#.##
-##.#################
-#####.##.###..####..
-..######..##.#######
-####.##.####...##..#
-.#####..#.######.###
-##...#.##########...
-#.##########.#######
-.####.#.###.###.#.##
-....##.##.###..#####
-.#.#.###########.###
-#.#.#.#####.####.###
-###.##.####.##.#..##";
+        let input = INPUT_BIG;
         let asteroids = get_asteroids(input);
         let visible_asteroids = get_visible_asteroids(&asteroids);
         assert_eq!(visible_asteroids.iter().max(), Some(&210));
+    }
+
+    #[test]
+    fn test_200th_vaporized() {
+        let input = INPUT_BIG;
+        let asteroids = get_asteroids(input);
+        let visible = get_visible_asteroids(&asteroids);
+        let (pos, _) = visible.iter().enumerate().max_by_key(|v| v.1).unwrap();
+        let battle_station = asteroids[pos];
+
+        let mut asteroids = asteroids;
+        asteroids.remove(pos);
+
+        let asteroid = get_200th_vaporized(asteroids, battle_station);
+        assert_eq!(asteroid, Pos::new(8, 2));
     }
 }
