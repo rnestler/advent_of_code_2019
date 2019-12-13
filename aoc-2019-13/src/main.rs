b6b4aea04d1e0465a2ebcf0ecf7b5fcfab38da84
Raphael Nestler <raphael.nestler@gmail.com>
2019-12-13 08:18:28 +0100

    Add solution for day 13 part 1

---
+++ aoc-2019-13/src/main.rs
@@ -0,0 +1,84 @@
+use intcode_computer::Machine;
+use std::collections::HashMap;
+
+#[derive(PartialEq, Eq, Debug, Copy, Clone)]
+enum Tile {
+    Empty = 0,
+    Wall = 1,
+    Block = 2,
+    HorizontalPaddle = 3,
+    Ball = 4,
+}
+
+impl From<i64> for Tile {
+    fn from(value: i64) -> Self {
+        match value {
+            0 => Tile::Empty,
+            1 => Tile::Wall,
+            2 => Tile::Block,
+            3 => Tile::HorizontalPaddle,
+            4 => Tile::Ball,
+            _ => panic!("unknown tile"),
+        }
+    }
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
+struct Pos {
+    x: i64,
+    y: i64,
+}
+impl Pos {
+    pub fn new(x: i64, y: i64) -> Self {
+        Pos { x, y }
+    }
+}
+struct Arcade {
+    pub machine: Machine,
+    pub screen: HashMap<Pos, Tile>,
+    pub ball: Pos,
+    //pub walls: HashSet<Pos>,
+    //pub blocks: HashSet<Pos>,
+    //pub paddles: HashSet<Pos>,
+    //pub balls: HashSet<Pos>,
+}
+
+impl Arcade {
+    pub fn new(machine: Machine) -> Arcade {
+        Arcade {
+            machine,
+            screen: HashMap::new(),
+            ball: Pos::new(0, 0),
+        }
+    }
+
+    pub fn run(&mut self) {
+        let _ = self.machine.run_until_block();
+        let output = self.machine.drain_output();
+        for chunk in output.chunks_exact(3) {
+            let pos = Pos::new(chunk[0], chunk[1]);
+            let tile = Tile::from(chunk[2]);
+            if tile != Tile::Empty {
+                if tile == Tile::Ball {
+                    println!("ball at {:?}", pos);
+                    self.ball = pos;
+                }
+            }
+            if tile == Tile::Block {
+                self.screen.insert(pos, tile);
+            }
+        }
+    }
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let code = Machine::read_code("input.txt")?;
+    let mut arcade = Arcade::new(Machine::new(code));
+
+    arcade.run();
+
+    let result = arcade.screen.keys().count();
+    println!("result part1: {}", result);
+
+    Ok(())
+}
