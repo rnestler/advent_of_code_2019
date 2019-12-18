0884343f8a54b88ab52c3bac5fea0a488a453145
Raphael Nestler <raphael.nestler@gmail.com>
2019-12-18 08:56:15 +0100

    Add project for day 18

---
+++ aoc-2019-18/src/main.rs
@@ -0,0 +1,14 @@
+use std::fs::File;
+use std::io::prelude::*;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut file = File::open("input.txt")?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+
+    for line in contents.lines() {
+        println!("{}", line);
+    }
+
+    Ok(())
+}
